02ec60bbef30e4807bc4b00dbf2b01bc9fe1c98f
taifu-bao <ha.quoc.bao.73@shiftinc.jp>
2023-11-01 19:05:04 +0900

    fix

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,11 +4,8 @@ import * as dotenv from 'dotenv';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import morgan from 'morgan';
-import { errorHandler } from './lib/middleware/error-handler-middleware.ts';
-import { requestInfoManager } from './lib/request-info-manager.ts';
 
 //import router
-import { router as healthCheck } from './features/health-check/index.ts';
 
 dotenv.config({ path: `.env.${process.env.NODE_ENV || 'local'}` });
 
@@ -18,19 +15,15 @@ const app: Express = express();
 app.use(httpContext.middleware);
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(requestInfoManager.middleware);
+
 app.use(express.json());
 app.use(cors());
 app.set('port', process.env.PORT || 8080);
 //Subsidy Router
-app.use('/', healthCheck);
 
 //Not in routes
 app.use(function (_req: Request, res: Response) {
   res.status(404).json({ error: '404 not found' });
 });
-
-// error handler
-app.use(errorHandler);
 
 export default app;
